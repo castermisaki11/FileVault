f99cdeadd87246bdb1cffcb855769124118bfe1f
CasterMisaki <castermisaki11@gmail.com>
2026-04-11 18:43:26 +0700

    update

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Server is running");
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 function startServer(port) {
